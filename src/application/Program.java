7f0aa2cb607dbd40d93b26a096891ec219c9b2b3
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-10-29 20:37:28 -0300

    update implementation.

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,10 +37,18 @@ public class Program {
 		{
 		    System.out.println(obj);
 		}
+
 	    System.out.println(" ===TEST 4: seller insert ====");
 	    Seller newSeller = new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, department);
 	    sellerDao.insert(newSeller);
 	    System.out.println("Inserted! New id = " + newSeller.getId());
+
+	    System.out.println(" ===TEST 5: seller update ====");
+
+	    seller = sellerDao.findById(1);
+	    seller.setName("Martha Waine");
+	    sellerDao.update(seller);
+	    System.out.println("Update completed!");
 
 	}
 
